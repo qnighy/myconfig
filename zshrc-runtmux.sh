f34961f52f9964dc78d7f353325dbddb5f2112af
Masaki Hara <ackie.h.gmai@gmail.com>
2017-01-12 16:18:00 +0900

    Introduce better condition for zshrc-runtmux.

--- zshrc-runtmux.sh
+++ zshrc-runtmux.sh
@@ -1,8 +1,14 @@
-if [[ ! -n "${TMUX-}" ]]; then
+#!/usr/bin/env zsh
+
+# If $SSH_CONNECTION is defined but $TMUX not yet defined, launch tmux.
+if [[ -z "${TMUX-}" && -n "${SSH_CONNECTION-}" ]]; then
+  # Try to find existing session
   if tmux has-session; then
+    # If there are, use the session.
     tmux attach-session || exit $?
     exit 0
   else
+    # If there isn't, create one.
     tmux || exit $?
     exit 0
   fi
